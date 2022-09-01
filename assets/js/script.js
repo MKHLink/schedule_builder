fc084e66db68a3bf6198df064863851a785d1a69
Aleph Null <likhonhasankomool@gmail.com>
2022-09-01 16:59:07 -0400

    early prototype of save events

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,31 @@
 var date = moment().format('dddd');
 $("#currentDay").append(date);
+
+
+//var taskEvent=[];
+//event creator
+
+/*$(".task").on("click", function(){
+    var text = $(this).text().trim();
+    var textInput = $("<textarea>").addClass("col-6 text").val(text);
+    $(this).replaceWith(textInput);
+    textInput.trigger("focus");
+});*/
+
+
+//save task
+
+$("#save").on("click",function()
+{
+    var task = document.getElementById("todo").value;
+    localStorage.setItem("todo",task);
+    alert("Saved");
+    console.log(task);
+});
+
+loadEvents();
+
+function loadEvents()
+{
+    document.getElementById("todo").value=localStorage.getItem("todo");
+}
