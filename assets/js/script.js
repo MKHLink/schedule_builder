ab032189ac2fb9f62a1cfbfc64eac62d0fb83a8d
Aleph Null <likhonhasankomool@gmail.com>
2022-09-01 17:31:14 -0400

    task insertion and saving/loading from local storage done

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,17 +15,76 @@ $("#currentDay").append(date);
 
 //save task
 
-$("#save").on("click",function()
+$("#save0").on("click",function()
 {
-    var task = document.getElementById("todo").value;
-    localStorage.setItem("todo",task);
+    var task = document.getElementById("todo0").value;
+    localStorage.setItem("todo0",task);
     alert("Saved");
-    console.log(task);
 });
+
+$("#save1").on("click",function()
+{
+    var task = document.getElementById("todo1").value;
+    localStorage.setItem("todo1",task);
+    alert("Saved");
+});
+
+$("#save2").on("click",function()
+{
+    var task = document.getElementById("todo2").value;
+    localStorage.setItem("todo2",task);
+    alert("Saved");
+});
+
+$("#save3").on("click",function()
+{
+    var task = document.getElementById("todo3").value;
+    localStorage.setItem("todo3",task);
+    alert("Saved");
+});
+
+$("#save4").on("click",function()
+{
+    var task = document.getElementById("todo4").value;
+    localStorage.setItem("todo4",task);
+    alert("Saved");
+});
+
+$("#save5").on("click",function()
+{
+    var task = document.getElementById("todo5").value;
+    localStorage.setItem("todo5",task);
+    alert("Saved");
+});
+
+$("#save6").on("click",function()
+{
+    var task = document.getElementById("todo6").value;
+    localStorage.setItem("todo6",task);
+    alert("Saved");
+});
+
+$("#save7").on("click",function()
+{
+    var task = document.getElementById("todo7").value;
+    localStorage.setItem("todo7",task);
+    alert("Saved");
+});
+
+$("#save8").on("click",function()
+{
+    var task = document.getElementById("todo8").value;
+    localStorage.setItem("todo8",task);
+    alert("Saved");
+});
+
 
 loadEvents();
 
 function loadEvents()
 {
-    document.getElementById("todo").value=localStorage.getItem("todo");
+    for(var i = 0;i<9;i++)
+    {
+        document.getElementById("todo"+i).value=localStorage.getItem("todo"+i);
+    }
 }
